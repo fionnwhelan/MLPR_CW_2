fb70997d7ea0a5a8bf0214cbef84766f8b7b410d
Fionn <fionnwhelan1@gmail.com>
2024-10-28 21:25:09 +0000

    finished question 1

---
+++ main.py
@@ -0,0 +1,4 @@
+import numpy as np
+data = np.load('ct_data.npz')
+X_train = data['X_train']; X_val = data['X_val']; X_test = data['X_test']
+y_train = data['y_train']; y_val = data['y_val']; y_test = data['y_test']
